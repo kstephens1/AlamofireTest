31677f3e94ce85bced09cae2b7d169f23e974de2
Keith Stephens <kstephens1@gmail.com>
2017-10-18 21:14:28 +0100

    Initial Commit

---
+++ AlamoFireTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamoFireTest
+//
+//  Created by Keith Stephens on 18/10/2017.
+//  Copyright © 2017 Keith Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
