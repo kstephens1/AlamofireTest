6223ee3bba0b9347d0ab89c3e2461e5bebbea9ef
Keith Stephens <kstephens1@gmail.com>
2017-11-19 21:50:02 +0000

    Initial Project

--- AlamoFireTest/ViewController.swift
+++ AlamoFireTest/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+//        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+//            print("Request: \(String(describing: response.request))")   // original url request
+//            print("Response: \(String(describing: response.response))") // http url response
+//            print("Result: \(response.result)")                         // response serialization result
+//
+//            if let json = response.result.value {
+//                print("JSON: \(json)") // serialized json response
+//            }
+//
+//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+//                print("Data: \(utf8Text)") // original server data as UTF8 string
+//            }
+//        }
+        
+//        Alamofire.request("https://httpbin.org/get").validate().responseJSON { response in
+//            switch response.result {
+//            case .success:
+//                print("Validation Successful")
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+        
+        
+        
     }
+
+    @IBAction func btnTest(_ sender: Any) {
+    
+        //from https://stackoverflow.com/questions/26114831/how-to-parse-json-response-from-alamofire-api-in-swift
+        Alamofire.request("https://jsonplaceholder.typicode.com/posts/1").responseJSON { response in
+            //debugPrint(response)
+            
+            if let json = response.data {
+                let data = JSON(data: json)
+                
+                let timestamp = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .medium, timeStyle: .long)
+                
+                self.txtView.text = ""
+                self.txtView.text = timestamp
+                self.txtView.text.append(data["title"].string!)
+                self.txtView.text.append(data["body"].string!)
+            
+            }
+        }
+        
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var txtView: UITextView!
+    
 }
 
